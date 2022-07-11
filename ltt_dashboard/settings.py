15ebe05ac463b817985018fa84d8fe0b374f142d
Chetan <chetanverma19@gmail.com>
2022-07-12 02:09:05 +0530

    Quickfix trial

--- ltt_dashboard/settings.py
+++ ltt_dashboard/settings.py
@@ -184,6 +184,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = env.str('EMAIL_HOST_USER', default='admin')
 EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD', default='admin')
 
-import django_on_heroku
-django_on_heroku.settings(locals(), db_ssl_required=False)
+# import django_on_heroku
+# django_on_heroku.settings(locals(), db_ssl_required=False)
 
